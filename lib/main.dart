5fd6dad2e99bae5e95b2732eeed08090c4d54811
vpsines <sasip6028@gmail.com>
2023-02-10 00:25:18 +0530

    feat: add routes and initial routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tic_tac_toe/providers/room_provider.dart';
+import 'package:tic_tac_toe/screens/create_room_screen.dart';
+import 'package:tic_tac_toe/screens/game_screen.dart';
+import 'package:tic_tac_toe/screens/join_room_screen.dart';
+import 'package:tic_tac_toe/screens/main_screen.dart';
 import 'package:tic_tac_toe/utils/colors.dart';
 
 void main() {
@@ -11,10 +17,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(backgroundColor: backgroundColor),
-      home: Container(),
+    return ChangeNotifierProvider(
+      create: (context) => RoomProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark().copyWith(backgroundColor: backgroundColor),
+        initialRoute: MainScreen.routeName,
+        routes: {
+          MainScreen.routeName: (context) => const MainScreen(),
+          JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+          CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
+          GameScreen.routeName: (context) => const GameScreen()
+        },
+      ),
     );
   }
 }
