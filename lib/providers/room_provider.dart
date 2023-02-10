d634b7e7c2c2a3b3ca94e6f7e1ef1562cf2dd2ea
vpsines <sasip6028@gmail.com>
2023-02-10 12:50:59 +0530

    feat: add board elements to room provider

--- lib/providers/room_provider.dart
+++ lib/providers/room_provider.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/foundation.dart';
 import 'package:tic_tac_toe/models/player.dart';
 
-class RoomProvider extends ChangeNotifier{
-  Map<String,dynamic> _roomData = {};
-  Player _player1 = Player(nickName: '', socketId: '', points: 0, playerType: 'X');
-  Player _player2 = Player(nickName: '', socketId: '', points: 0, playerType: 'O');
+class RoomProvider extends ChangeNotifier {
+  Map<String, dynamic> _roomData = {};
+
+  List<String> _boardElements = List<String>.filled(9, '');
+
+  Player _player1 =
+      Player(nickName: '', socketId: '', points: 0, playerType: 'X');
+  Player _player2 =
+      Player(nickName: '', socketId: '', points: 0, playerType: 'O');
+
+  int _filledBoxes =0;
+
 
   Player get player1 => _player1;
   Player get player2 => _player2;
 
-  Map<String,dynamic> get roomData => _roomData;
+  Map<String, dynamic> get roomData => _roomData;
 
-  void updateRoomData(Map<String,dynamic> data){
+  List<String> get boardElements => _boardElements;
+
+  void updateRoomData(Map<String, dynamic> data) {
     _roomData = data;
     notifyListeners();
   }
 
- void updatePlayer1(Map<String,dynamic> playerData){
+  void updatePlayer1(Map<String, dynamic> playerData) {
     _player1 = Player.fromMap(playerData);
     notifyListeners();
   }
 
-  void updatePlayer2(Map<String,dynamic> playerData){
+  void updatePlayer2(Map<String, dynamic> playerData) {
     _player2 = Player.fromMap(playerData);
+    notifyListeners();
+  }
+
+  void updateBoardElements(int index, String choice) {
+    _boardElements[index] = choice;
+    _filledBoxes += 1;
     notifyListeners();
   }
 }
