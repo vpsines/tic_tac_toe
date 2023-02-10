5022d6bda770526f26f162ee112a5fce330539ae
vpsines <sasip6028@gmail.com>
2023-02-10 12:49:44 +0530

    feat: add socket getter and onTapBoard event

--- lib/resources/socket_methods.dart
+++ lib/resources/socket_methods.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:socket_io_client/socket_io_client.dart';
 import 'package:tic_tac_toe/resources/socket_client.dart';
 import 'package:tic_tac_toe/screens/game_screen.dart';
 import 'package:tic_tac_toe/providers/room_provider.dart';
 import 'package:tic_tac_toe/utils/util.dart';
 
 class SocketMethods {
-  final socketClient = SocketClient.instance.socket;
+  final _socketClient = SocketClient.instance.socket;
+
+  Socket get socketClient => _socketClient!;
 
   void createRoom(String nickName) {
     if (nickName.isNotEmpty) {
-      socketClient!.emit('createRoom', {'nickName': nickName});
+      _socketClient!.emit('createRoom', {'nickName': nickName});
     }
   }
 
   void joinRoom(String nickName, String roomId) {
     if (nickName.isNotEmpty && roomId.isNotEmpty) {
-      socketClient!.emit('joinRoom', {'nickName': nickName, 'roomId': roomId});
+      _socketClient!.emit('joinRoom', {'nickName': nickName, 'roomId': roomId});
+    }
+  }
+
+  void onBoardTap(int index, String roomId, List<String> boardElements) {
+    if (boardElements[index].isEmpty) {
+      _socketClient!.emit('boardTap', {'index': index, 'roomId': roomId});
     }
   }
 
   void createRoomSuccessListener(BuildContext context) {
-    socketClient!.on('createRoomSuccess', (room) {
+    _socketClient!.on('createRoomSuccess', (room) {
       Provider.of<RoomProvider>(context, listen: false).updateRoomData(room);
-      print("room created :${room}");
       Navigator.pushNamed(context, GameScreen.routeName);
     });
   }
 
   void joinRoomSuccessListener(BuildContext context) {
-    socketClient!.on('joinRoomSuccess', (room) {
+    _socketClient!.on('joinRoomSuccess', (room) {
       Provider.of<RoomProvider>(context, listen: false).updateRoomData(room);
       print("join successfull :${room}");
       Navigator.pushNamed(context, GameScreen.routeName);
@@ -37,13 +45,13 @@ class SocketMethods {
   }
 
   void errorListener(BuildContext context) {
-    socketClient!.on('errorOccured', (message) {
+    _socketClient!.on('errorOccured', (message) {
       showSnackbar(context, message);
     });
   }
 
   void playerStateUpdateListener(BuildContext context) {
-    socketClient!.on('playersUpdated', (playerData) {
+    _socketClient!.on('playersUpdated', (playerData) {
       Provider.of<RoomProvider>(context, listen: false)
           .updatePlayer1(playerData[0]);
       Provider.of<RoomProvider>(context, listen: false)
@@ -51,9 +59,17 @@ class SocketMethods {
     });
   }
 
-    void updateRoomListener(BuildContext context) {
-    socketClient!.on('updateRoom', (room) {
+  void updateRoomListener(BuildContext context) {
+    _socketClient!.on('updateRoom', (room) {
       Provider.of<RoomProvider>(context, listen: false).updateRoomData(room);
+    });
+  }
+
+  void boardTapListener(BuildContext context){
+    _socketClient!.on('boardTapped', (data){
+      RoomProvider roomProvider = Provider.of<RoomProvider>(context, listen: false);
+      roomProvider.updateBoardElements(data['index'], data['choice']);
+      roomProvider.updateRoomData(data['room']);
     });
   }
 }
