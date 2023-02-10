58a721fd437ad094c1cbd3b7c6b6d478fa1969e1
vpsines <sasip6028@gmail.com>
2023-02-10 15:00:38 +0530

    feat: add show dialog

--- lib/utils/util.dart
+++ lib/utils/util.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/resources/game_methods.dart';
 
 void showSnackbar(BuildContext context, String content) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(content)));
+}
+
+void showGameDialog(BuildContext context, String text) {
+  showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(text),
+        actions: [
+          TextButton(onPressed: () {
+            GameMethods().resetBoard(context);
+            Navigator.of(context).pop();
+          }, child: const Text('Play again'))
+        ],
+      );
+    },
+  );
 }
