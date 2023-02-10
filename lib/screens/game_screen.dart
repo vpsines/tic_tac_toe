32e9a455d74707cc34c4e39b604200f076bec6d7
vpsines <sasip6028@gmail.com>
2023-02-10 12:50:30 +0530

    feat: add game screen

---
+++ lib/screens/game_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tic_tac_toe/providers/room_provider.dart';
+import 'package:tic_tac_toe/resources/socket_methods.dart';
+import 'package:tic_tac_toe/widgets/score_board.dart';
+import 'package:tic_tac_toe/widgets/tic_tac_toe_board.dart';
+import 'package:tic_tac_toe/widgets/waiting_lobby.dart';
+
+class GameScreen extends StatefulWidget {
+  static const String routeName = '/game';
+
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  final SocketMethods socketMethods = SocketMethods();
+
+  @override
+  void initState() {
+    super.initState();
+    socketMethods.updateRoomListener(context);
+    socketMethods.playerStateUpdateListener(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<RoomProvider>(
+      builder: (context, roomProvider, child) {
+        return Scaffold(
+          body: (roomProvider.roomData['isJoin'])
+              ? const WaitingLobby()
+              : SafeArea(child: 
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                    ScoreBoard(),
+                    TicTacToeBoard()
+                ],
+              )),
+        );
+      },
+    );
+  }
+}
